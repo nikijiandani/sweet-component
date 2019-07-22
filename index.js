7f1aec5a65ee797dc1b54d5aecc18b86f9f0e82f
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-22 16:12:26 -0400

    rename index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const React = require('react')
-
-module.exports = props => 
-<div>
-  `This is <b>really sweet</b> ${props.emoji && '😍'}`
-  `This is also a really sweet ${props.emoji && '😍'}`
-</div>
